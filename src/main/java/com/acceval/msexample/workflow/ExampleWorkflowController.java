52c36357b0394cd9beb2441870dbb8985544101c
Julian Ooi <julianooi@gmail.com>
2018-12-15 00:04:07 +0800

    Workflow stuff.

--- src/main/java/com/acceval/msexample/workflow/ExampleWorkflowController.java
+++ src/main/java/com/acceval/msexample/workflow/ExampleWorkflowController.java
@@ -1,9 +1,11 @@
 package com.acceval.msexample.workflow;
 
 import com.acceval.workflow.client.Workflow;
+import com.acceval.workflow.client.WorkflowRegistrar;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,14 +15,17 @@ public class ExampleWorkflowController {
 	private final Workflow workflow1;
 	private final Workflow workflow2;
 	private final Workflow workflow3;
+	private final WorkflowRegistrar registrar;
 
 	public ExampleWorkflowController(
 			@Qualifier(WorkflowQualifiers.EXAMPLE_WORKFLOW_1) Workflow workflow1,
 			@Qualifier(WorkflowQualifiers.EXAMPLE_WORKFLOW_2) Workflow workflow2,
-			@Qualifier(WorkflowQualifiers.EXAMPLE_WORKFLOW_3) Workflow workflow3) {
+			@Qualifier(WorkflowQualifiers.EXAMPLE_WORKFLOW_3) Workflow workflow3,
+			WorkflowRegistrar registrar) {
 		this.workflow1 = workflow1;
 		this.workflow2 = workflow2;
 		this.workflow3 = workflow3;
+		this.registrar = registrar;
 	}
 
 	@PostMapping(path = "/start1")
@@ -36,6 +41,11 @@ public class ExampleWorkflowController {
 	@PostMapping(path = "/start3")
 	public void startWorkflow3(@RequestBody ExampleWorkflowData data) {
 		workflow3.start(data);
+	}
+
+	@GetMapping(path = "/register")
+	public void registerWorkflow() throws IOException {
+		registrar.registerWorkflow(workflow1);
 	}
 
 	@GetMapping(path = "/taskList")
